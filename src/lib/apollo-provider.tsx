8fcd811b6975189b3918d5d6008b9870370a8052
henrique weiand <henriqueweiand@gmail.com>
2025-03-20 20:19:08 -0300

    added env for GRAPHQL_URL

--- src/lib/apollo-provider.tsx
+++ src/lib/apollo-provider.tsx
@@ -4,12 +4,11 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
 import { ReactNode } from 'react';
 
-// uri: 'http://localhost:3000/graphql',
 export function ApolloWrapper({ children }: { children: ReactNode }) {
   const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: createUploadLink({
-      uri: 'https://nestjs-exam-ai.onrender.com/graphql',
+      uri: process.env.GRAPHQL_URL || 'http://localhost:3000/graphql',
       headers: {
         'apollo-require-preflight': 'true',
       }
